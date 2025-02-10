4805f5bf68fbee0f54b5e61b6d4f70f4dad6f5b9
Yevhen <benatanlev@icloud.com>
2025-02-10 14:02:35 +0200

    sum

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,25 +1,48 @@
 import flatpickr from 'flatpickr';
 import iziToast from 'izitoast';
+import { convertMs } from './convert-ms';
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+const startBtn = document.querySelector('[data-start]');
+const inputData = document.getElementById('datetime-picker');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+let userSelectedDate;
 
-  return { days, hours, minutes, seconds };
-}
+flatpickr(inputData, options);
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+startBtn.addEventListener('click', () => {
+  console.log('start');
+});
+
+function addLeadingZero(value) {}
+
+// const interval = setInterval(() => {
+//   console.log(Date.now());
+// });
+
+document.addEventListener('click', () => {
+  clearInterval(interval);
+});
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+const date = new Date('2025-12-12');
+
+console.log(date.getTime());
+console.dir(date);
+
+const newDate = {
+  name: date,
+};
+
+console.log(newDate);
