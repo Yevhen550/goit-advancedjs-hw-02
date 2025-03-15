f4ab81dbd9812a6a73f8b837353744bab5a5438e
Yevhen <benatanlev@icloud.com>
2025-03-15 10:12:07 +0200

    snackbar is done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,38 @@
 import iziToast from 'izitoast';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitBtn);
+
+function onSubmitBtn(ev) {
+  ev.preventDefault();
+
+  const state = form.elements.state.value;
+  const delay = parseInt(form.elements.delay.value, 10);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(() => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(() => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+}
